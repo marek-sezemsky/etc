1794e10324e4e2c4d7de5f0d56f9ee185d44ba4f
Marek Sezemsky <marek.sezemsky@gmail.com>
2013-03-11 23:58:46 +0100

    better install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 #
-# Install marek-etc files as symlinks in current directory. Overwrite existing
-# symlinks.
+# Install listed $1/ files as hidden symlinks into current directory.
+# Overwrite existing symlinks.
 #
+# cd $HOME && marek-etc/install.sh marek-etc/
 #
 
 set -e
 set -u
 #set -x
 
+# files to install
+files="bashrc vimrc"
+
 if [ -z "${1:-}" ]; then
     echo "Usage: $0 <src>" >&2
     exit 1
+else
+    src=$1
 fi
 
-src=$1
 
 link()
 {
@@ -30,7 +35,6 @@ link()
 }
 
 
-files="bashrc vimrc"
 for file in $files; do
     link $file
 done
