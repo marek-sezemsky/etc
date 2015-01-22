80c6bb1a6a9a604c0a2aa8d34a79d0b58fa592c9
Sezemský Marek <sezemsky@avast.com>
2015-01-22 13:05:57 +0100

    add ArgumentParser to python template

--- vim/templates/template.py
+++ vim/templates/template.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python
 """
 """
+import argparse
+import logging
 
-def main():
-    pass
+def parse_arguments():
+    parser = argparse.ArgumentParser(description=__doc__)
+    # ...
+
+    parser.add_argument('-d','--debug',
+            help='Print lots of debugging statements',
+            action="store_const", dest="loglevel", const=logging.DEBUG,
+            default=logging.WARNING)
+    parser.add_argument('-v','--verbose',
+            help='Be verbose',
+            action="store_const",dest="loglevel",const=logging.INFO)
+
+    return parser.parse_args()
+
+def main(args):
+    logging.debug("Debug message")
+    logging.info("Info message")
+    logging.warning("Warninig message")
 
 
 if __name__ == '__main__':
-    main()
+    args = parse_arguments()
+    logging.basicConfig(level=args.loglevel,
+            format='{}: %(levelname)s: %(msg)s'.format(__file__))
+    main(args)
