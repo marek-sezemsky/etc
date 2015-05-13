33ab124ea998e13ad82b382c2a96d3b4401f72e6
Marek Sezemsky <marek.sezemsky@gmail.com>
2015-05-13 20:51:32 +0200

    Add argparse to python VIM template

--- vim/templates/template.py
+++ vim/templates/template.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 """
 """
+import argparse
 
-def main():
+def main(args):
     pass
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+            description='Process some integers')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+',
+            help='an integer for the accumulator')
+    parser.add_argument('--sum', dest='accumulate', action='store_const',
+            const=sum, default=max,
+            help='sum the integers (default: find the max)')
+
+    main(parser.parse_args())
