539108d9d03f09ec7f9451ca5c59cf1468f21c96
Marek Sezemský <marek.sezemsky@gmail.com>
2023-06-16 15:37:55 +0200

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,120 +1,202 @@
-#Require 5.1
+#Requires -Version 5.1
 
-# source Python environment and patch the powerprompt function back to our version
-function se {
-    if ( Test-Path -Path "env\scripts" ) {
-        Write-Host "Loading Python from env ..."
-        . env\scripts\activate
-        powerprompt_patch
-    } elseif ( Test-Path -Path "venv\scrits" ) {
-        Write-Host "Loading Python from venv ..."
-        . venv\scripts\activate
-        powerprompt_patch
-    } else {
-        Write-Warning "No Python virtual env found (env, venv)!"
+# TODO split into profile.ps1, profile-prompt.ps1, profile-functions.ps1 ??
+
+
+# https://www.splunk.com/en_us/blog/tips-and-tricks/powershell-profiles-and-add-path.html
+function Add-Path {
+  <#
+  .SYNOPSIS
+  Adds a Directory to the Current Path
+  .DESCRIPTION
+  Add a directory to the current path.  This is useful for 
+  temporary changes to the path or, when run from your 
+  profile, for adjusting the path within your powershell 
+  prompt.
+  .EXAMPLE
+  Add-Path -Directory "C:\Program Files\Notepad++"
+  .PARAMETER Directory
+  The name of the directory to add to the current path.
+  #>
+  
+  [CmdletBinding()]
+  param (
+  [Parameter(
+  Mandatory=$True,
+  ValueFromPipeline=$True,
+  ValueFromPipelineByPropertyName=$True,
+  HelpMessage='What directory would you like to add?')]
+  [Alias('dir')]
+  [string[]]$Directory
+  )
+  
+  PROCESS {
+    $Path = $env:PATH.Split(';')
+    
+    foreach ($dir in $Directory) {
+      if ($Path -contains $dir) {
+        Write-Verbose "$dir is already present in PATH"
+      } else {
+        if (-not (Test-Path $dir)) {
+          Write-Verbose "$dir does not exist in the filesystem"
+        } else {
+          $Path += $dir
+        }
+      }
     }
+    
+    $env:PATH = [String]::Join(';', $Path)
+  }
 }
 
-# PS1 PowerPrompt mimics bash_ps1;
 
+#
+# "se" -- source (Python) environmnt
+#
+# Interactive function to:
+# - source first found Python ["venv", "env"] in current directory
+# - patch the powerprompt function back to our version to revert venv's default prompt addition of "(venv)" prefix
+#
+function se() {
+  $tryEnvDirs = @( "venv", "env" )
+  $found = 0;
+  
+  foreach ($dir in $tryEnvDirs ) {
+    if ( Test-Path -Path "${dir}\scripts\activate" ) {
+      $found = 1;
+      Write-Host "Sourcing Python VirtualEnv: ${dir}"
+      Write-Host
+      .$dir\scripts\activate
+      # TODO: observed on some(?) Pythons that venvs still fuck op the propmt with "se"?!
+      powerprompt_patch
+      break
+    } else {
+      Write-Warning "Not a Python VirtualEnv in path: ${dir}"
+    }
+  }
+  
+  if ( ! $found ) {
+    throw "No Python VirtualEnv found, tried: $tryEnvDirs";
+  }
+}
+
+#
+# PS1 PowerPrompt
+#
+# Windows (lite) version of bash_ps1;
+#
+
+# check if current user is admin
 function is_admin() {
-    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
-    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) 
+  $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+  (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) 
 }
 
 function powerprompt {
-    $__prev=$?
-
-    # make a room (empty line)
-    Write-Host
-
-    # --- # admin!
-    if ( is_admin ) {
-        Write-Host -ForegroundColor Gray -NoNewline "# "
-        Write-Host -ForegroundColor Red "Administrator"
-    }
-
-    # --- '# proxy ..."
-    if ( $env:ALL_PROXY ) {
-        Write-Host -ForegroundColor Gray -NoNewline "# all proxy "
-        Write-Host -ForegroundColor Cyan $env:ALL_PROXY
-    }
-
-    # --- '# envs ..."
-    if ( $env:VIRTUAL_ENV ) {
-        Write-Host -NoNewline -ForegroundColor Gray  "# pyvenv "
-        Write-Host -ForegroundColor Cyan $env:VIRTUAL_ENV
-    }
-    
-    # [kubectx]
-    $K8sContext=$(Get-Content ~/.kube/config | Select-String -Pattern "current-context: (.*)")
-    If ($K8sContext) {
-        $ctx = $K8sContext.Matches[0].Groups[1].Value
-        Write-Host -NoNewline -ForegroundColor Gray  "# kubectx "
-        if ($K8sContext -imatch "admin") {
-            Write-Host -ForegroundColor Red "Administrator" "$ctx"
-        } else {
-            Write-Host -ForegroundColor Yellow "$ctx"
-        }
-    }        
-    
-    # --- '# git ...'
-    # TODO!?
-    
-    # [user]@[Host] C:\Dir\Subdir
+  # remember status of just finished command
+  $__prev=$?
+  
+  # make a room since last printout (empty line)
+  Write-Host
+  
+  # --- # admin!
+  if ( is_admin ) {
     Write-Host -ForegroundColor Gray -NoNewline "# "
-    Write-Host -ForegroundColor Green -NoNewline $env:USERNAME
-    Write-Host -ForegroundColor Gray -NoNewline "@"
-    Write-Host -ForegroundColor Green -NoNewline $env:COMPUTERNAME
-    Write-Host -ForegroundColor Gray -NoNewline " "
-    Write-Host $executionContext.SessionState.Path.CurrentLocation
+    Write-Host -ForegroundColor Red "Administrator"
+  }
+  
+  # --- '# proxy ..."
+  if ( $env:ALL_PROXY ) {
+    Write-Host -ForegroundColor Gray -NoNewline "# "
+    Write-Host -ForegroundColor Cyan -NoNewline "webproxy "
+    Write-Host -ForegroundColor Magenta "${env:ALL_PROXY}"
+  }
+  
+  # --- '# KUBECONFIG  ..."
+  if ( $env:KUBECONFIG ) {
+    Write-Host -ForegroundColor Gray -NoNewline "#"
+    Write-Host -ForegroundColor Blue -NoNewline " k8s "
+    Write-Host -ForegroundColor Gray -NoNewline " KUBECONFIG "
+    Write-Host -ForegroundColor Blue $env:KUBECONFIG 
+  }
+  
+  $kubectx=$(kubectx --current 2>$null)
+  If ($kubectx) {
+    $kubens=$(kubens --current 2>$null)
+    Write-Host -ForegroundColor Gray -NoNewline "# "
+    Write-Host -ForegroundColor Blue -NoNewline "k8s "
+    Write-Host -ForegroundColor Gray -NoNewline "ns "
+    Write-Host -ForegroundColor Yellow -NoNewline $kubens
+    Write-Host -ForegroundColor Gray -NoNewline " ctx "
+    Write-Host -ForegroundColor Yellow -NoNewline $kubectx
 
-        # TODO!?
-    
-    # # --- '# time retval \$' # dík Pane Klobouk 🕑
-    # # this is poor man's timer
-    # local T=${SECONDS}
-    # local M=$((T/60%60))
-    # local S=$((T%60))
-    # SECONDS=0
-    # PS1="${PS1}# ${timecolor}$(date +'%a%d %H%M%S')${__c_off} ${M}m${S}s${warningcolor}~${__c_off} "
-    # if [ $retval -eq 0 ]; then
-    #     PS1="${PS1}${successcolor}$retval ✓ "
-    # else
-    #     PS1="${PS1}${failurecolor}$retval ✗ "
-    # fi
-    # PS1="${PS1}${prompt}${__c_off} "
-
-    # ... behold prompt!
-    #     C:\>_
-    $ps = "PS$('>' * ($nestedPromptLevel + 1))"
-    if ( $__prev ) {
-        Write-Host -NoNewline -ForegroundColor Green "."
-        Write-Host -NoNewline " $ps"
-    } else {
-        Write-Host -NoNewline -ForegroundColor Red "X $ps"
+    if ( ($kubectx -imatch "admin") -Or ($kubens -imatch "kube-") ) {
+      Write-Host -ForegroundColor Red -NoNewline " Administrator"
     }
-
-    return " "
+    Write-Host 
+  }        
+  
+  # --- '# envs ..."
+  if ( $env:VIRTUAL_ENV ) {
+    Write-Host -NoNewline -ForegroundColor Gray  "# pyvenv "
+    Write-Host -ForegroundColor Cyan $env:VIRTUAL_ENV
+  }
+  
+  # --- '# git ...'
+  # TODO!? gitposh
+  
+  # [user]@[Host] C:\Dir\Subdir
+  Write-Host -ForegroundColor Gray -NoNewline "# "
+  Write-Host -ForegroundColor Green -NoNewline $env:USERNAME
+  Write-Host -ForegroundColor Gray -NoNewline "@"
+  Write-Host -ForegroundColor Green -NoNewline $env:COMPUTERNAME
+  Write-Host -ForegroundColor Gray -NoNewline " "
+  Write-Host $executionContext.SessionState.Path.CurrentLocation
+  
+  # TODO!?
+  
+  # # --- '# time retval \$' # dík Pane Klobouk 🕑
+  # # this is poor man's timer
+  # local T=${SECONDS}
+  # local M=$((T/60%60))
+  # local S=$((T%60))
+  # SECONDS=0
+  # PS1="${PS1}# ${timecolor}$(date +'%a%d %H%M%S')${__c_off} ${M}m${S}s${warningcolor}~${__c_off} "
+  # if [ $retval -eq 0 ]; then
+  #     PS1="${PS1}${successcolor}$retval ✓ "
+  # else
+  #     PS1="${PS1}${failurecolor}$retval ✗ "
+  # fi
+  # PS1="${PS1}${prompt}${__c_off} "
+  
+  # ... behold prompt!
+  #     C:\>_
+  $ps = "PS$('>' * ($nestedPromptLevel + 1))"
+  if ( $__prev ) {
+    Write-Host -NoNewline -ForegroundColor Green "."
+    Write-Host -NoNewline " $ps"
+  } else {
+    Write-Host -NoNewline -ForegroundColor Red "X $ps"
+  }
+  
+  return " "
 }
-
-# function prompt {
-#     powerprompt
-# }
 
 # patch powerprompt
 function powerprompt_patch {
-    Write-Host "Patching prompt function."
-    copy-item function:powerprompt function:prompt
+  copy-item function:powerprompt function:prompt
 }
+
 
 
 ### profile()
 
-# function kubectl_complete()
-Write-Host "Loading 'kubectl' completion."
-kubectl completion powershell | Out-String | Invoke-Expression
+# setup fancy menued auto complete
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
+Write-Host "Loading 'posh-git' module."
+Import-Module posh-git
+
+Write-Host "Patching prompt function."
 powerprompt_patch
-
-
+  
