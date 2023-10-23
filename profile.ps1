6b3eee0b0f51a4ee600a3fb1bf0933d5711c2fc6
Marek Sezemský <marek.sezemsky@gmail.com>
2023-10-23 17:06:30 +0200

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -101,29 +101,37 @@ function powerprompt {
   
   # --- # admin!
   if ( is_admin ) {
-    Write-Host -ForegroundColor Gray -NoNewline "# "
-    Write-Host -ForegroundColor Red "Administrator"
+    Write-Host -ForegroundColor DarkGray -NoNewline "# "
+    Write-Host -ForegroundColor DarkRed "Administrator"
   }
   
   # --- '# proxy ..."
   if ( $env:ALL_PROXY ) {
-    Write-Host -ForegroundColor Gray -NoNewline "# "
+    Write-Host -ForegroundColor DarkGray -NoNewline "# "
     Write-Host -ForegroundColor Cyan -NoNewline "webproxy "
     Write-Host -ForegroundColor Magenta "${env:ALL_PROXY}"
   }
   
-  # --- '# KUBECONFIG  ..."
+  # --- '# Python venvs ..."
+  if ( $env:VIRTUAL_ENV ) {
+    Write-Host -NoNewline -ForegroundColor Gray  "# "
+    Write-Host -NoNewline -ForegroundColor Yellow "Python venv "
+    Write-Host -ForegroundColor Cyan $env:VIRTUAL_ENV
+  }
+
+  # --- '# k8s KUBECONFIG  ..."
   if ( $env:KUBECONFIG ) {
-    Write-Host -ForegroundColor Gray -NoNewline "#"
-    Write-Host -ForegroundColor Blue -NoNewline " k8s "
-    Write-Host -ForegroundColor Gray -NoNewline " KUBECONFIG "
+    Write-Host -ForegroundColor DarkGray -NoNewline "# "
+    Write-Host -ForegroundColor LightBlue -NoNewline "k8s "
+    Write-Host -ForegroundColor Gray -NoNewline "KUBECONFIG "
     Write-Host -ForegroundColor Blue $env:KUBECONFIG 
   }
-  
+
+  # --- '# k8s ns ... ctx ... (admin)"
   $kubectx=$(kubectx --current 2>$null)
   If ($kubectx) {
     $kubens=$(kubens --current 2>$null)
-    Write-Host -ForegroundColor Gray -NoNewline "# "
+    Write-Host -ForegroundColor DarkGray -NoNewline "# "
     Write-Host -ForegroundColor Blue -NoNewline "k8s "
     Write-Host -ForegroundColor Gray -NoNewline "ns "
     Write-Host -ForegroundColor Yellow -NoNewline $kubens
@@ -134,23 +142,53 @@ function powerprompt {
       Write-Host -ForegroundColor Red -NoNewline " Administrator"
     }
     Write-Host 
-  }        
-  
-  # --- '# envs ..."
-  if ( $env:VIRTUAL_ENV ) {
-    Write-Host -NoNewline -ForegroundColor Gray  "# pyvenv "
-    Write-Host -ForegroundColor Cyan $env:VIRTUAL_ENV
   }
-  
-  # --- '# git ...'
+
+  # --- '# git <(__git_ps1) # (commit msg)...'
   # TODO!? gitposh
+  if ($status = Get-GitStatus -Force) {
+    $branch = $status.Branch;
+    # Write-Host $status
+    Write-Host -ForegroundColor DarkGray -NoNewline "# "
+    Write-Host -ForegroundColor DarkRed -NoNewline "git"
+    if ($status.HasWorking) {
+      $hash = $( git rev-parse --short HEAD )
+      if ( $hash ) {
+        Write-Host -ForegroundColor Cyan -NoNewline " $hash"
+      }
+
+      $branch = $status.Branch;
+      if ( $branch ) {
+        Write-Host -ForegroundColor DarkYellow -NoNewline " ${branch}"
+      }
+      
+      $gitstr="";
+      $gitstr += $(Write-GitWorkingDirStatusSummary $status -NoLeadingSpace)
+      $gitstr += $(Write-GitWorkingDirStatus $status)
+      if ( $gitstr ) {
+        Write-Host -ForegroundColor DarkYellow -NoNewline " ${gitstr}"
+      }
+      Write-Host 
+    }
   
+  #   Write-Host working and index
+  #   if ($status.HasWorking -and $status.HasIndex) {
+  #   }
+  #   Write-Host index
+  #   if ($status.HasIndex) {
+  #     Write-GitIndexStatus $status -NoLeadingSpace
+  #   }
+  #   Write-Host branch status
+  #   Write-GitBranchStatus $status -NoLeadingSpace
+  #   Write-GitBranchName $status
+  }
+
   # [user]@[Host] C:\Dir\Subdir
-  Write-Host -ForegroundColor Gray -NoNewline "# "
+  Write-Host -ForegroundColor DarkGray -NoNewline "# "
   Write-Host -ForegroundColor Green -NoNewline $env:USERNAME
-  Write-Host -ForegroundColor Gray -NoNewline "@"
+  Write-Host -ForegroundColor DarkGray -NoNewline "@"
   Write-Host -ForegroundColor Green -NoNewline $env:COMPUTERNAME
-  Write-Host -ForegroundColor Gray -NoNewline " "
+  Write-Host -ForegroundColor DarkGray -NoNewline " "
   Write-Host $executionContext.SessionState.Path.CurrentLocation
   
   # TODO!?
@@ -178,7 +216,8 @@ function powerprompt {
   } else {
     Write-Host -NoNewline -ForegroundColor Red "X $ps"
   }
-  
+
+  # --- '# versions' for DEBUG
   return " "
 }
 
